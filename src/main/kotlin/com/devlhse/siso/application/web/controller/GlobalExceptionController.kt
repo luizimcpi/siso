9b78e8c3a9f82431c2c06eb7e702aa268e8b13ef
Luiz Henrique <luizhenrique.se@gmail.com>
2020-10-31 19:44:45 -0300

    adicionando tratamento para informações da request

--- src/main/kotlin/com/devlhse/siso/application/web/controller/GlobalExceptionController.kt
+++ src/main/kotlin/com/devlhse/siso/application/web/controller/GlobalExceptionController.kt
@@ -1,20 +1,43 @@
 package com.devlhse.siso.application.web.controller
 
+import com.devlhse.siso.domain.extensions.removeQuotation
 import com.devlhse.siso.domain.model.response.GenericResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import javax.validation.ConstraintViolationException
 
 @RestControllerAdvice
 class GlobalExceptionController {
 
-    @ExceptionHandler(value = [ConstraintViolationException::class])
-    fun validationHandler(constraintViolationException: ConstraintViolationException): GenericResponse<String> {
-        return GenericResponse(
-                code = HttpStatus.BAD_REQUEST.value(),
-                status = HttpStatus.BAD_REQUEST.toString(),
-                data = constraintViolationException.message!!
+    val log: Logger = LoggerFactory.getLogger(GlobalExceptionController::class.java)
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun validationHandler(exception: MethodArgumentNotValidException): ResponseEntity<GenericResponse<MutableList<String>>> {
+        log.error("Errors: ${exception.message}")
+
+        val fieldErrors = exception.bindingResult.fieldErrors
+        val globalErrors = exception.bindingResult.globalErrors
+
+        val resultErrors = mutableListOf<String>()
+
+        for(error in fieldErrors){
+            resultErrors.add("${error.field}: ${error.defaultMessage?.removeQuotation()}")
+        }
+
+        for(error in globalErrors){
+            resultErrors.add("${error.objectName}: ${error.defaultMessage?.removeQuotation()}")
+        }
+
+        return ResponseEntity.badRequest().body(
+                GenericResponse(
+                    code = HttpStatus.BAD_REQUEST.value(),
+                    status = HttpStatus.BAD_REQUEST.toString(),
+                    data = resultErrors
+                )
         )
     }
 }
