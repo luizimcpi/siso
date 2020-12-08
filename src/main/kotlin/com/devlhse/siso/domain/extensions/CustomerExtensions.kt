16ea8892c8fbbc1f490d25ad98840d8b05e48a49
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2020-12-08 16:15:28 -0300

    Adicionando annotations para geração automatica de valores na entidade

--- src/main/kotlin/com/devlhse/siso/domain/extensions/CustomerExtensions.kt
+++ src/main/kotlin/com/devlhse/siso/domain/extensions/CustomerExtensions.kt
@@ -5,26 +5,21 @@ import com.devlhse.siso.domain.model.request.CustomerRequest
 import com.devlhse.siso.domain.model.response.CustomerResponse
 import com.devlhse.siso.domain.util.Constants.NO_DOCUMENT
 import com.devlhse.siso.domain.util.Constants.NO_PHONE
-import java.time.LocalDateTime
-import java.util.UUID
 
 fun CustomerRequest.toEntity(userId: Long): Customer =
         Customer(
-                id = UUID.randomUUID(),
                 userId = userId,
                 name = this.name!!,
                 email = this.email!!,
                 mobilePhone = this.mobilePhone.toString(),
                 phone = this.phone ?: NO_PHONE,
                 birthDate = this.birthDate,
-                document = this.document ?: NO_DOCUMENT,
-                createdAt = LocalDateTime.now(),
-                updatedAt = LocalDateTime.now()
+                document = this.document ?: NO_DOCUMENT
         )
 
 fun Customer.toResponse(): CustomerResponse =
         CustomerResponse(
-                id = this.id,
+                id = this.id!!,
                 userId = userId,
                 name = this.name,
                 email = this.email,
@@ -32,6 +27,6 @@ fun Customer.toResponse(): CustomerResponse =
                 phone = this.phone,
                 birthDate = this.birthDate,
                 document = this.document,
-                createdAt = this.createdAt,
-                updatedAt = this.updatedAt
+                createdAt = this.createdAt!!,
+                updatedAt = this.updatedAt!!
         )
--- src/main/kotlin/com/devlhse/siso/domain/model/entity/Customer.kt
+++ src/main/kotlin/com/devlhse/siso/domain/model/entity/Customer.kt
@@ -1,30 +1,48 @@
 package com.devlhse.siso.domain.model.entity
 
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "customer")
 data class Customer(
+
         @Id
-        val id: UUID,
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: UUID? = null,
+
         @Column(name = "user_id")
         val userId: Long,
+
         val name: String,
+
         val email: String,
+
         @Column(name = "mobile_phone")
         val mobilePhone: String,
+
         val phone: String,
+
         @Column(name = "birth_date")
         val birthDate: LocalDate,
+
         val document: String,
-        @Column(name = "created_at")
-        val createdAt: LocalDateTime,
+
+        @Column(name = "created_at", updatable = false)
+        @CreationTimestamp
+        val createdAt: LocalDateTime? = null,
+
         @Column(name = "updated_at")
-        val updatedAt: LocalDateTime
+        @UpdateTimestamp
+        val updatedAt: LocalDateTime? = null
 )
